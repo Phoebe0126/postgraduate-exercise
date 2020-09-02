b2afea20cc4e493425635a9ae3c8bac023d9f557
Phoebe0126 <1542756558@qq.com>
2020-09-02 20:32:44 +0800

    add notice bar

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,20 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
+		<!-- 公告栏 -->
+		<view class="notice-wrapper">
+			<uni-notice-bar 
+				class="notice"
+				scrollable="true" 
+				single="true" 
+				text="[单行] 这是 NoticeBar 通告栏，这是 NoticeBar 通告栏，这是 NoticeBar 通告栏"
+				background-color="#9f8080"
+				showIcon="true"
+				speed="50"
+				color="#fff"
+			>
+			</uni-notice-bar>
+		</view>
+
 		<view>
 			<text class="title">{{title}}</text>
 			<button @click="login">登录</button>
@@ -11,83 +25,84 @@
 
 <script>
 import { getUserOpenId } from '../../api/user.js';
+import uniNoticeBar from '@/components/uni-notice-bar/uni-notice-bar/uni-notice-bar.vue';
 
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-			this.login();
-		},
-		methods: {
-			login () {
-				wx.login({
-					success (res) {
-						if (res.code) {
-						// 获取用户的openId
-						getUserOpenId({
-							appid: 'wx0e8cbbba3aab1125',
-							secret: '9097a462abccba0564091d8536fc7295',
-							js_code: res.code,
-							grant_type: 'authorization_code'
-						}).then(res => {
-							// 存储openId
-							getApp().globalData.openID = res.openid;
+export default {
+	data() {
+		return {
+			title: 'Hello'
+		}
+	},
+	components: {
+		uniNoticeBar
+	},
+	onLoad() {
+		this.login();
+	},
+	methods: {
+		login () {
+			wx.login({
+				success (res) {
+					if (res.code) {
+					// 获取用户的openId
+					getUserOpenId({
+						appid: 'wx0e8cbbba3aab1125',
+						secret: '9097a462abccba0564091d8536fc7295',
+						js_code: res.code,
+						grant_type: 'authorization_code'
+					}).then(res => {
+						// 存储openId
+						getApp().globalData.openID = res.openid;
 
-							
-							console.log(getApp().globalData.openID);
-						})
 						
-						} else {
-						console.log('登录失败！' + res.errMsg)
-						}
-					},
-					fail (err) {
-						console.log(err,2);
+						console.log(getApp().globalData.openID);
+					})
+					
+					} else {
+					console.log('登录失败！' + res.errMsg)
 					}
-				})
-			},
-			getUserInfo (res) {
-				this.login();
-				// todo 发起后端请求
-				// 根据返回的数据传输相应的用户信息
-				if (res.detail.userInfo) {
-					const { avatarUrl, nickName, gender } = res.detail.userInfo;
-					const params = {
-						openID: getApp().globalData.openID,
-						avatar: avatarUrl,
-						gender,
-						nickname: nickName
-					}
+				},
+				fail (err) {
+					console.log(err,2);
 				}
-				console.log(res);
+			})
+		},
+		getUserInfo (res) {
+			this.login();
+			// todo 发起后端请求
+			// 根据返回的数据传输相应的用户信息
+			if (res.detail.userInfo) {
+				const { avatarUrl, nickName, gender } = res.detail.userInfo;
+				const params = {
+					openID: getApp().globalData.openID,
+					avatar: avatarUrl,
+					gender,
+					nickname: nickName
+				}
 			}
+			console.log(res);
 		}
 	}
+}
 </script>
 
 <style lang="scss">
 	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		.logo {
-			height: 200rpx;
-			width: 200rpx;
-			margin: 200rpx auto 50rpx auto;
+		.notice-wrapper {
+			padding: 20rpx 0;
+			background: #c9a2a2;
+			.notice {
+				width: 100%;
+				.uni-noticebar {
+					padding: 8rpx 10rpx !important;
+				}
+				.uni-noticebar__content-text {
+					font-size: 24rpx!important;
+					font-weight: 100;
+				}
+				
+			}
 		}
 	}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
